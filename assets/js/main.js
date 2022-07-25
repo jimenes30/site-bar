a68acd587d1450b69183871a586cb882d7a97a4e
jimenes30 <alfredojimenes99@gmail.com>
2022-07-25 00:17:56 -0300

    versão 1.01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log('Teste Console')
+var botaoSaibaMais = document.querySelector('.banner__botao')
+var nav_sobre = document.querySelector('#sobre')
+function rolar_para_sobre() {
+  scrollTo(0, 663)
+}
+
+botaoSaibaMais.addEventListener('click', rolar_para_sobre)
+nav_sobre.addEventListener('click', rolar_para_sobre)
